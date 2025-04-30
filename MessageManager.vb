01094d90061c95b59547c88360838ca17775b8cc
AdamSattler <adamsat@seznam.cz>
2025-04-30 10:22:48 +0200

    first commit

---
+++ MessageManager.vb
@@ -0,0 +1,254 @@
+﻿Imports LokiLoad.Test.GSMConnector.GSM
+
+Public Class MessageManager
+    Private Shared Property ATSLogin As String = System.Configuration.ConfigurationManager.AppSettings("ATSLogin")
+
+    Private Shared Property ATSPassword As String = System.Configuration.ConfigurationManager.AppSettings("ATSPassword")
+    
+    Private Shared Property DaktelaLogin As String = System.Configuration.ConfigurationManager.AppSettings("DaktelaLogin")
+    
+    Private Shared Property DaktelaPassword As String = System.Configuration.ConfigurationManager.AppSettings("DaktelaPassword")
+    
+    Public Shared Property UseDaktela As Boolean
+    
+    Private Shared Sub SendSMSMessage(ByVal myMessage As Message)
+        Dim debugMode As Boolean = False
+        Dim debugTo() As String = {}
+
+        If System.Configuration.ConfigurationManager.AppSettings("DebugMode") IsNot Nothing AndAlso System.Configuration.ConfigurationManager.AppSettings("DebugMode").ToLowerInvariant = "true" AndAlso System.Configuration.ConfigurationManager.AppSettings("Messages.GSM.Debug.To") IsNot Nothing AndAlso Not String.IsNullOrEmpty(System.Configuration.ConfigurationManager.AppSettings("Messages.GSM.Debug.To")) Then
+            debugMode = True
+            debugTo = System.Configuration.ConfigurationManager.AppSettings("Messages.GSM.Debug.To").Replace(",", ";").Split(";")
+        End If
+
+        Dim finalTarget As New List(Of String)
+
+        If debugMode Then
+
+            For Each t As String In debugTo
+                finalTarget.Add(t)
+            Next
+
+        Else
+            finalTarget.Add(myMessage.Target)
+
+            If Not String.IsNullOrEmpty(myMessage.TargetCC) Then
+                finalTarget.Add(myMessage.TargetCC)
+            End If
+
+            If Not String.IsNullOrEmpty(myMessage.TargetBCC) Then
+                finalTarget.Add(myMessage.TargetBCC)
+            End If
+
+        End If
+
+
+        'INFO: Odesilam podle SMSGate
+        If myMessage.SmsGate.HasValue AndAlso myMessage.SmsGate.Value = MessageSmsGate.ATS Then
+            'INFO: Výběr GSM Brány
+            Dim ATSGSMLogin As String = ATSLogin
+            Dim ATSGSMPassword As String = ATSPassword
+
+            Using service As New GSMConnector.GSM.GSMServiceSoapClient ' GSMConnector.GSM.GMSServiceSoapClient()
+
+                For Each target As String In finalTarget
+
+                    Try
+                        myMessage.DateOfSend = Now
+                        Dim resSendSMS As GSMConnector.GSM.WSMessageSendResponse = service.SendSMS(ATSGSMLogin, ATSGSMPassword, target, myMessage.Body, "", "", "", True, Nothing, Nothing)
+                        If resSendSMS.Result Then
+
+                            myMessage.ExternalId = resSendSMS.IdMessage
+
+                            myMessage.Status = MessageStatus.Sending
+                            MessageManager.Save(myMessage, False)
+                            Logger.GetDefaultLogger.Write(LoggerMessageLevel.Info, "SMS X-Message-ID={0} to {1} send by ATS.", myMessage.Id, target)
+
+                        Else
+                            'Log("Zprava nebyla odeslana Result: " + resSendSMS.ResultMessage)
+                            myMessage.Status = MessageStatus.Error
+                            MessageManager.Save(myMessage, False)
+
+                            Logger.GetDefaultLogger.Write(LoggerMessageLevel.Error, "Error sending SMS X-Message-ID={0} by ATS to {1}, {2}", myMessage.Id, target, resSendSMS.ResultMessage)
+                        End If
+
+
+                    Catch ex As Exception
+                        If ex.ToString.Contains("Nepodarilo se odeslat SMS Ex: Telefon nema nutnych 9 znaku") Then
+                            Logger.GetDefaultLogger.Write(LoggerMessageLevel.Info, "Message Id={0} to {1} - " + ex.ToString, myMessage.Id, target)
+
+                            myMessage.Status = MessageStatus.Error
+                            MessageManager.Save(myMessage, False)
+                        Else
+                            Throw ex
+                        End If
+                    End Try
+
+
+                Next
+
+            End Using
+        ElseIf myMessage.SmsGate.HasValue AndAlso myMessage.SmsGate.Value = MessageSmsGate.Daktela Then
+            'INFO: Výběr GSM Brány
+            Dim DaktelaGSMLogin As String = DaktelaLogin
+            Dim DaktelaGSMPassword As String = DaktelaPassword
+
+            Using service As New GSMConnector.GSM.GSMServiceSoapClient ' GSMConnector.GSM.GMSServiceSoapClient()
+
+                For Each target As String In finalTarget
+
+                    Try
+                        myMessage.DateOfSend = Now
+                        Dim resSendSMS As GSMConnector.GSM.WSMessageSendResponse = service.SendSMS(DaktelaGSMLogin, DaktelaGSMPassword, target, myMessage.Body, "", "", "", True, Nothing, Nothing)
+                        If resSendSMS.Result Then
+
+                            myMessage.ExternalId = resSendSMS.IdMessage
+
+                            myMessage.Status = MessageStatus.Sending
+                            MessageManager.Save(myMessage, False)
+                            Logger.GetDefaultLogger.Write(LoggerMessageLevel.Info, "SMS X-Message-ID={0} to {1} send by ATS.", myMessage.Id, target)
+
+                        Else
+                            'Log("Zprava nebyla odeslana Result: " + resSendSMS.ResultMessage)
+                            myMessage.Status = MessageStatus.Error
+                            MessageManager.Save(myMessage, False)
+
+                            Logger.GetDefaultLogger.Write(LoggerMessageLevel.Error, "Error sending SMS X-Message-ID={0} by ATS to {1}, {2}", myMessage.Id, target, resSendSMS.ResultMessage)
+                        End If
+
+
+                    Catch ex As Exception
+                        If ex.ToString.Contains("Nepodarilo se odeslat SMS Ex: Telefon nema nutnych 9 znaku") Then
+                            Logger.GetDefaultLogger.Write(LoggerMessageLevel.Info, "Message Id={0} to {1} - " + ex.ToString, myMessage.Id, target)
+
+                            myMessage.Status = MessageStatus.Error
+                            MessageManager.Save(myMessage, False)
+                        Else
+                            Throw ex
+                        End If
+                    End Try
+
+
+                Next
+
+            End Using
+        Else
+
+
+
+            'INFO: Výběr GSM Brány
+            Dim GSMLogin As String
+            Dim GSMPassword As String
+
+            If myMessage.Kind.HasValue AndAlso myMessage.Kind.Value = MessageKind.SMSCampaign Then
+                Dim simSetting As SimSettings = SimSettingsManager.GetSimSettingForMessage(myMessage.Environment, SimSettingsType.Campaigne)
+                If simSetting IsNot Nothing Then
+                    myMessage.GatewayId = simSetting.GatewayId
+                    GSMLogin = simSetting.Login
+                    GSMPassword = simSetting.Password
+                Else
+
+                    Logger.GetDefaultLogger.Write(LoggerMessageLevel.Error, "Error sending SMS - Not more SIM Campaigne space. Message-ID={0} to {1}", myMessage.Id, myMessage.Target)
+                    Throw New Exception("Error sending SMS - Not more SIM Campaigne space.")
+                End If
+
+                'If MaxCampaignSMS < TodaySendSMSCampaign(myMessage.Environment) Then
+                '    Exit Sub
+                'End If
+                'Dim gateWay As Integer = GetLastUsedGatewayCampaign(myMessage.Environment) + 1 - 100
+                'If gateWay > NumberOfCampaignGateway OrElse gateWay = -99 Then gateWay = 1
+                'myMessage.GatewayId = gateWay + 100
+                'GSMLogin = GetSMSLoginForGatewayCampaign(gateWay)
+                'GSMPassword = GetSMSPasswordForGatewayCampaign(gateWay)
+            Else
+                Dim simSetting As SimSettings = SimSettingsManager.GetSimSettingForMessage(myMessage.Environment, SimSettingsType.Standard)
+                If simSetting IsNot Nothing Then
+                    myMessage.GatewayId = simSetting.GatewayId
+                    GSMLogin = simSetting.Login
+                    GSMPassword = simSetting.Password
+                Else
+                    If UseDaktela Then
+                        GSMLogin = DaktelaLogin
+                        GSMPassword = DaktelaPassword
+                        myMessage.GatewayId = 100
+                    Else
+                        Logger.GetDefaultLogger.Write(LoggerMessageLevel.Error, "Error sending SMS - Not more SIM Standard space. Message-ID={0} to {1}", myMessage.Id, myMessage.Target)
+                        Throw New Exception("Error sending SMS - Not more SIM Standard space.")
+                    End If
+                End If
+
+
+                'Dim daktela As Boolean = False
+                'If UseDaktela AndAlso MaxStandardSMS.HasValue Then
+                '    If MaxStandardSMS < TodaySendSMS(myMessage.Environment) Then
+                '        daktela = True
+                '    End If
+                'End If
+                'If daktela Then
+                '    GSMLogin = DaktelaLogin
+                '    GSMPassword = DaktelaPassword
+                '    myMessage.GatewayId = 100
+                'Else
+
+
+                '    SimSettingsManager.
+                '    Dim gateWay As Integer = GetLastUsedGateway(myMessage.Environment) + 1
+                '    ' Logger.GetDefaultLogger.Write(LoggerMessageLevel.Info, "SendSMSMessage - GateWay1=" + gateWay.ToString + ", NumberOfStandardGateway=" + NumberOfStandardGateway.ToString)
+                '    If gateWay > NumberOfStandardGateway Then gateWay = 1
+                '    ' Logger.GetDefaultLogger.Write(LoggerMessageLevel.Info, "SendSMSMessage - GateWayFinal=" + gateWay.ToString)
+                '    myMessage.GatewayId = gateWay
+                '    GSMLogin = GetSMSLoginForGateway(gateWay)
+                '    GSMPassword = GetSMSPasswordForGateway(gateWay)
+                'End If
+            End If
+
+
+
+            Using service As New GSMConnector.GSM.GSMServiceSoapClient ' GSMConnector.GSM.GMSServiceSoapClient()
+
+                For Each target As String In finalTarget
+
+                    Try
+                        myMessage.DateOfSend = Now
+                        Dim resSendSMS As GSMConnector.GSM.WSMessageSendResponse = service.SendSMS(GSMLogin, GSMPassword, target, myMessage.Body, "", "", "", True, Nothing, Nothing)
+                        If resSendSMS.Result Then
+
+                            myMessage.ExternalId = resSendSMS.IdMessage
+
+                            myMessage.Status = MessageStatus.Sending
+                            MessageManager.Save(myMessage, False)
+                            Logger.GetDefaultLogger.Write(LoggerMessageLevel.Info, "SMS X-Message-ID={0} to {1} send.", myMessage.Id, target)
+
+                        Else
+                            'Log("Zprava nebyla odeslana Result: " + resSendSMS.ResultMessage)
+                            myMessage.Status = MessageStatus.Error
+                            MessageManager.Save(myMessage, False)
+
+                            Logger.GetDefaultLogger.Write(LoggerMessageLevel.Error, "Error sending SMS X-Message-ID={0} to {1}, {2}", myMessage.Id, target, resSendSMS.ResultMessage)
+                        End If
+
+
+                    Catch ex As Exception
+                        If ex.ToString.Contains("Nepodarilo se odeslat SMS Ex: Telefon nema nutnych 9 znaku") Then
+                            Logger.GetDefaultLogger.Write(LoggerMessageLevel.Info, "Message Id={0} to {1} - " + ex.ToString, myMessage.Id, target)
+
+                            myMessage.Status = MessageStatus.Error
+                            MessageManager.Save(myMessage, False)
+                        Else
+                            Throw ex
+                        End If
+                    End Try
+
+
+                Next
+
+            End Using
+        End If
+
+
+    End Sub
+    
+
+    Private Shared Function Save(myMessage As Message, b As Boolean) As WSMessageSendResponse
+        Throw New NotImplementedException
+    End Function
+End Class
---
+++ README.md
@@ -0,0 +1,80 @@
+# VB.NET Code Refactoring Challenge – `SendSMSMessage`
+
+Welcome to the Code Refactoring Challenge! This Visual Basic project is part of our developer interview process. The goal is to evaluate your ability to improve and modernize existing code through thoughtful refactoring.
+
+---
+
+## 🎯 Objective
+
+Your task is to **refactor the `SendSMSMessage` function** in the `MessageManager.vb` class.
+
+We’re looking for clean, idiomatic, and maintainable code that preserves the original behavior of the function.
+
+---
+
+## ✅ What We're Looking For
+
+We'll evaluate your submission based on:
+
+- **Code clarity and readability**
+- **Modular design and separation of concerns**
+- **Use of appropriate VB.NET / .NET Framework features**
+- **Maintainability and simplicity**
+- **Correctness** – the function must behave the same after refactoring
+- **Presence of a working unit test**
+
+---
+
+## 🧪 Testing Requirement
+
+You must write at least **one unit test** that verifies the behavior of `SendSMSMessage` before and after your changes.
+
+- You can use any .NET Framework-compatible testing framework (e.g., MSTest, NUnit, xUnit)
+- Place your test class in a separate folder (e.g., `/Tests`)
+- Include clear input/output assertions to show the function still behaves as expected
+
+> 💡 Feel free to mock any external dependencies or outputs.
+
+---
+
+## 🚀 How to Run
+
+This is a **.NET Framework** project. To build and run:
+
+1. Open `LokiLoad.Test.sln` in **Visual Studio 2019 or later**
+2. Build the solution
+3. Run your test using the Test Explorer in Visual Studio or IDE of your choice
+
+> 🧰 If needed, you can add a new test project to the solution.
+
+---
+
+## 📬 Submission Instructions
+
+You can submit your work in one of the following ways:
+
+### Option 1: GitHub Fork
+1. Fork this repository
+2. Commit your changes and unit test(s)
+3. Share the link to your repository
+
+### Option 2: ZIP Archive
+1. Zip the entire project folder (including your refactored code and unit tests)
+2. Include a brief `RefactoringNotes.txt` or update this README with:
+    - A summary of your improvements
+    - Any assumptions or design decisions
+3. Send the archive as instructed
+
+---
+
+## 🧠 Tips
+
+- Small, meaningful changes go a long way
+- Add helper methods or new classes if needed
+- Comment where clarification is helpful, but clean code should speak for itself
+- Don't over-engineer—keep things maintainable and understandable
+
+---
+
+Thanks for taking the time to complete this challenge. We’re excited to see how you approach improving real-world code!
+
